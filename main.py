02421c5adda91d8ea9606833b522082b0fbc9927
Shigeo Kitamura <shigeo9462@gmail.com>
2024-10-10 14:29:35 -0700

    add function prototype

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+To-do List Application
+"""
+
+to_do_list = []
+
+def add_task() -> None:
+    pass
+
+def remove_task() -> None:
+    pass
+
+def view_task() -> None:
+    pass
+
+def exit_app() -> None:
+    pass
